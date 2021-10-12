b4c231813044e9d8ecaf13b1b4c4586f105037f1
3DudesMedia <hthighway@zenterprise.org>
2021-10-12 21:20:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,45 +5,50 @@
 # $4 = TDL
 # $5 = ssh git location of your healthcheck script
 # $6 = Node Name
+
+zerosslemail="${1}"
+cfemail="${2}"
+cfapi="${3}"
+domain="${4}"
+health="${5}"
+node="${6}"
+
 setupsshkey () {
-hostname=$(uname -n) 
-ssh-keygen -o -a 100 -t ed25519 -f /root/.ssh/id_ed25519 -C "$hostname" 
-echo "We now need to send your ssh key that was just generated to we can talk to your repositories on gitlab Enter the following line into your gitlab auth ssh keys at https://gitlab.zenterprise.org/-/profile/keys" 
-cat /root/.ssh/id_ed25519.pub
-read -p "Press enter to continue"
+    hostname=$(uname -n) 
+    ssh-keygen -o -a 100 -t ed25519 -f /root/.ssh/id_ed25519 -C "$hostname" 
+    echo "We now need to send your ssh key that was just generated to we can talk to your repositories on gitlab Enter the following line into your gitlab auth ssh keys at https://gitlab.zenterprise.org/-/profile/keys" 
+    cat /root/.ssh/id_ed25519.pub
+    read -p "Press enter to continue"
 }
-
-
 
 ## Kernel Things 
 setupkernel () {
-apt update -y
-apt upgrade -y 
-wget -O /usr/local/bin/ubuntu-mainline-kernel.sh https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh 
-chmod +x /usr/local/bin/ubuntu-mainline-kernel.sh 
-/usr/local/bin/ubuntu-mainline-kernel.sh -i 5.11.11 
-wait
+    apt update -y
+    apt upgrade -y 
+    wget -O /usr/local/bin/ubuntu-mainline-kernel.sh https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh 
+    chmod +x /usr/local/bin/ubuntu-mainline-kernel.sh 
+    /usr/local/bin/ubuntu-mainline-kernel.sh -i 5.11.11 
+    wait
 }
-
-
-
 
 #  install docker-ce
 #  https://github.com/hthighwaymonk/docker-install
-#
 setupdocker () {
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-wait
-
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sh get-docker.sh
+    wait
+    curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 }
-#
-#
 
-#
+setupdockercompose () {
+    echo "created and placed the docker-compose.yml to /opt/docker"
+    mkdir -p /opt/docker
+    cd /opt/docker
+    wget -q -O docker-compose.yml https://gitlab.zenterprise.org/hthighway/cdn-create-a-node/-/raw/main/docker-compose.yml
+}
+
 # set sysctl
 setupsysctl () {
 echo "net.ipv4.tcp_window_scaling=1
@@ -79,23 +84,6 @@ net.core.netdev_budget=50000
 net.core.netdev_budget_usecs=5000" > /etc/sysctl.conf
 /sbin/sysctl -p
 }
-#
-#
-
-setupdockercompose () {
-    echo "created and placed the docker-compose.yml to /opt/docker"
-    mkdir -p /opt/docker
-    cd /opt/docker
-    wget -q -O docker-compose.yml https://gitlab.zenterprise.org/hthighway/cdn-create-a-node/-/raw/main/docker-compose.yml
-}
-
-copyhealthcheck () {
-    echo "Copied the sample healthcheck to /opt/scripts"
-    mkdir -p /opt/scripts
-    cd /opt/scripts
-    wget -q -O cdn-node-healthcheck.sh.sample https://gitlab.zenterprise.org/hthighway/cdn-create-a-node/-/raw/main/cdn-node-healthcheck.sh.sample
-}
-
 
 setuptraefikfolder () {
     echo "Copied the dynamic.yml to /opt/traefik/dynamic_configs"
@@ -104,15 +92,12 @@ setuptraefikfolder () {
     wget -q -O dynamic.yml https://gitlab.zenterprise.org/hthighway/cdn-create-a-node/-/raw/main/dynamic.yml
 }
 
-
 setupmainyml () {
     echo "Copied the SSH git repo location for your main.yml to /opt/traefik/dynamic_configs"
     read -r traefikconfig_git
     mkdir -p /opt/traefik
     cd /opt/traefik
     git clone "$traefikconfig_git" dynamic_configs
-
-
     FILE=/opt/traefik/dynamic_configs/main.yml
     if [ -f "$FILE" ]; then
         fcheck=0
@@ -130,6 +115,49 @@ setupcron () {
     rm /tmp/mycron
 }
 
+setupzerossl ()
+{
+    #  setup zeroSLL eab credetials 
+    #  define CF email and global token
+    #  set TDL
+    #  all in  /opt/docker/.env
+    mkdir -p /opt/docker
+    tokens=$(curl -Ss "https://api.zerossl.com/acme/eab-credentials-email" --data "email=$zerosslemail")
+    kid=$(echo "$tokens}" | grep -o 'eab_kid.*' | cut -f2- -d:)
+    kid=${kid%,*}
+    key=$(echo "${tokens##*:}")
+    key=${key::-1}
+    #remove quotes
+    kid="${kid%\"}"
+    kid="${kid#\"}"
+    key="${key%\"}"
+    key="${key#\"}"
+
+    echo "CLOUDFLARE_EMAIL=$cfemail
+    CLOUDFLARE_API_KEY=$cfapi
+    TLD=$domain
+    zerossl_email=$zerosslemail
+    zerossl_kid=${kid}
+    zerossl_key=${key}" > /opt/docker/.env
+}
+
+healthcheck () {
+    apt install run-one
+    echo "Copy the SSH git repo location for your HealthCheck file"
+    mkdir -p /opt/scripts
+    cd /opt/scripts
+    git clone "$health" health
+
+    crontab -l > /tmp/mycron
+    echo '*/5 * * * * cd /opt/scripts/health &&  git pull >/dev/null 2>&1' >> /tmp/mycron
+    crontab /tmp/mycron
+    rm /tmp/mycron
+
+    crontab -l > /tmp/mycron
+    echo '*/1 * * * * run-one /bin/bash /opt/scripts/health/healthcheck.sh $node $domain > /dev/null 2>&1' >> /tmp/mycron
+    crontab /tmp/mycron
+    rm /tmp/mycron
+}
 
 setupkernel
 setupsshkey
@@ -144,51 +172,15 @@ else
 fi
 setuptraefikfolder
 setupcron
+setupzerossl
 
-#  setup zeroSLL eab credetials 
-#  define CF email and global token
-#  set TDL
-#  all in  /opt/docker/.env
-mkdir -p /opt/docker
-tokens=$(curl -Ss "https://api.zerossl.com/acme/eab-credentials-email" --data "email=$1")
-kid=$(echo "$tokens}" | grep -o 'eab_kid.*' | cut -f2- -d:)
-kid=${kid%,*}
-key=$(echo "${tokens##*:}")
-key=${key::-1}
-#remove quotes
-kid="${kid%\"}"
-kid="${kid#\"}"
-key="${key%\"}"
-key="${key#\"}"
-
-echo "CLOUDFLARE_EMAIL=$2
-CLOUDFLARE_API_KEY=$3
-TLD=$4
-zerossl_email=$1
-zerossl_kid=${kid}
-zerossl_key=${key}" > /opt/docker/.env
-
-
-
-if [ -z "$5" ]; then
+if [ -z "$health" ]; then
     :
 else
-    apt install run-one
-    echo "Copy the SSH git repo location for your HealthCheck file"
-    mkdir -p /opt/scripts
-    cd /opt/scripts
-    git clone "$5" health
-
-    crontab -l > /tmp/mycron
-    echo '*/5 * * * * cd /opt/scripts/health &&  git pull >/dev/null 2>&1' >> /tmp/mycron
-    crontab /tmp/mycron
-    rm /tmp/mycron
-
-    crontab -l > /tmp/mycron
-    echo '*/1 * * * * run-one /bin/bash /opt/scripts/health/healthcheck.sh $6 $4 > /dev/null 2>&1' >> /tmp/mycron
-    crontab /tmp/mycron
-    rm /tmp/mycron
+    healthcheck
 fi
+
+
 
 echo "=========================================="
 echo "=========================================="
